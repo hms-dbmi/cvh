4f8b484bc534570be4745e2e17765dfc891dd266
John Conroy <john-conroy@users.noreply.github.com>
2025-02-13 17:12:54 -0500

    Add universal route padding

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,13 +1,16 @@
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import Box from "@mui/material/Box";
+
 import Header from "../features/navigation/components/Header";
 
 export const Route = createRootRoute({
   component: () => (
     <>
       <Header />
-      <hr />
-      <Outlet />
+      <Box p={2}>
+        <Outlet />
+      </Box>
       <TanStackRouterDevtools />
     </>
   ),
