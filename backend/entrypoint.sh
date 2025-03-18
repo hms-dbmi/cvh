ff9abc76a4b6bef50f600e207b39282eff20f785
John Conroy <john-conroy@users.noreply.github.com>
2025-03-18 17:46:08 -0400

    Comment out ingest datasets

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 python ./core/manage.py migrate --noinput
-python ./core/manage.py ingest_datasets
+# python ./core/manage.py ingest_datasets
 python -m gunicorn --bind 0.0.0.0:8000 --chdir core core.wsgi:application
