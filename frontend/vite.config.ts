f411fa6adf7e14b3fccd843203b4617167ac1edf
John Conroy <john-conroy@users.noreply.github.com>
2025-03-21 14:42:39 -0400

    Update vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import svgr from "vite-plugin-svgr";
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true }),
+  plugins: [react(), TanStackRouterVite({ autoCodeSplitting: true }), svgr(),
   ],
 })
