83b946047747f7403a3818c978d7b8123c78b394
John Conroy <john-conroy@users.noreply.github.com>
2025-03-24 19:11:31 -0400

    Fix port

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 
 python ./core/manage.py migrate --noinput
 # python ./core/manage.py ingest_datasets
-python -m gunicorn --certfile=/etc/letsencrypt/live/34.227.19.8/fullchain.pem --keyfile=/etc/letsencrypt/live/34.227.19.8/privkey.pem --bind 0.0.0.0:443 --chdir core core.wsgi:application
+python -m gunicorn --certfile=/etc/letsencrypt/live/34.227.19.8/fullchain.pem --keyfile=/etc/letsencrypt/live/34.227.19.8/privkey.pem --bind 0.0.0.0:8000 --chdir core core.wsgi:application
