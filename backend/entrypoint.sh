994886ef45340e322704eb42dd8cf0b5b8c4cf43
John Conroy <john-conroy@users.noreply.github.com>
2025-03-24 22:51:34 -0400

    Remove cert

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,4 +2,4 @@
 
 python ./core/manage.py migrate --noinput
 # python ./core/manage.py ingest_datasets
-python -m gunicorn --certfile=./certbot/conf/live/vis-api.link/fullchain.pem --keyfile=./certbot/conf/live/vis-api.link/privkey.pem --bind 0.0.0.0:8000 --chdir core core.wsgi:application
+python -m gunicorn --bind 0.0.0.0:8000 --chdir core core.wsgi:application
