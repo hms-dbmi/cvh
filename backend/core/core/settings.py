84427e95aca16b5c33b60a4121622376756bace0
John Conroy <john-conroy@users.noreply.github.com>
2025-09-23 13:36:46 -0400

    Add logging to debug allowed hosts

--- backend/core/core/settings.py
+++ backend/core/core/settings.py
@@ -16,14 +16,15 @@ from pathlib import Path
 import boto3
 from botocore.exceptions import ClientError
 import json
+import logging
 
 env.read_env()
 # Override in .env for local development
 DEBUG = env.bool("DEBUG", default=False)
+logger = logging.getLogger(__name__)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -37,14 +38,17 @@ DEBUG = True
 ALLOWED_HOSTS = []
 METADATA_URI = env.str("ECS_CONTAINER_METADATA_URI")
 
+logger.info(METADATA_URI)
 if METADATA_URI:
     container_metadata = requests.get(METADATA_URI).json()
     ALLOWED_HOSTS.append(container_metadata["Networks"][0]["IPv4Addresses"][0])
 
 ENV_ALLOWED_HOSTS = env.str("ALLOWED_HOSTS").split(",")
+logger.info(env.str("ALLOWED_HOSTS"))
 
 if ENV_ALLOWED_HOSTS:
     ALLOWED_HOSTS.extend(ENV_ALLOWED_HOSTS)
+logger.info(ALLOWED_HOSTS)
 
 
 # Application definition
