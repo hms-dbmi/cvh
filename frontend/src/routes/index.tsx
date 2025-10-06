6f6c0bd14dbcb0701c7e2c0c1e0fc8e025633811
John Conroy <john-conroy@users.noreply.github.com>
2025-10-06 16:12:04 -0400

    Replace cvh homepage with placeholder

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -6,11 +6,9 @@ import Box from "@mui/material/Box";
 import ProjectsList, {
   PublicProjectsList,
 } from "../features/projects/components/ProjectsList";
-import PublishedVisualizationsList from "../features/visualizations/components/PublishedVisualizationsList";
 import DatasetsList from "../features/datasets/components/DatasetsList";
 import { LoginButton } from "../features/navigation/components/AuthButtons";
 import { Typography } from "@mui/material";
-import CFDEIcon from "../assets/cfde-vector.svg?react";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -28,12 +26,14 @@ function RouteComponent() {
 
 function AuthenticatedRouteComponent() {
   return (
-    <Box sx={{
-      display: 'grid',
-      gridTemplateColumns: { xs: '1fr',  lg: 'repeat(3, 1fr)' },
-      gap: 2,
-      padding: 2,
-    }}>
+    <Box
+      sx={{
+        display: "grid",
+        gridTemplateColumns: { xs: "1fr", lg: "repeat(3, 1fr)" },
+        gap: 2,
+        padding: 2,
+      }}
+    >
       <ProjectsList queryOptions={{ params: { query: { limit: 5 } } }} />
       <PublicProjectsList />
       <DatasetsList queryOptions={{ params: { query: { limit: 5 } } }} />
@@ -43,34 +43,20 @@ function AuthenticatedRouteComponent() {
 
 function UnauthenticatedRouteComponent() {
   return (
-    <Stack
-      direction="row"
-      spacing={10}
-      height="100%"
-      justifyContent="center"
-      alignItems="center"
-    >
+    <Stack height="100%" justifyContent="center" alignItems="center">
       <Stack spacing={5}>
-        <CFDEIcon height={250} />
         <Stack spacing={2} maxWidth={1000}>
           <Typography variant="h4" component="h1">
-            The CFDE Community Visualization Hub enables visualization-guided
-            evaluation of predictions, model refinement, and hypothesis
-            generation, fueling deeper insights into biological mechanisms
+            Create and Share Interactive Genomics Data Visualizations
           </Typography>
           <Typography variant="h5" component="p">
-            Log in to view projects, data sources and visualizations
+            Join our community of researchers and data scientists to explore,
+            visualize and collaborate on complex datasets.
           </Typography>
           <Box>
             <LoginButton variant="contained" color="primary" />
           </Box>
         </Stack>
-      </Stack>
-
-      <Stack direction="row" spacing={4}>
-        <PublishedVisualizationsList
-          queryOptions={{ params: { query: { limit: 5 } } }}
-        />
       </Stack>
     </Stack>
   );
