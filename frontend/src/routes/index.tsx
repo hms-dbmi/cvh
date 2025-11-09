4b14fb2f87d26228f09348e3d7b595fd1b531b45
John Conroy <john-conroy@users.noreply.github.com>
2025-11-09 18:56:12 -0500

    Update homepage

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -5,6 +5,7 @@ import { useAuth0 } from "@auth0/auth0-react";
 
 import { LoginButton } from "../features/navigation/components/AuthButtons";
 import { Typography } from "@mui/material";
+import PublishedVisualizationGrid from "../features/visualizations/components/PublishedVisualizationGrid";
 
 export const Route = createFileRoute("/")({
   component: RouteComponent,
@@ -14,23 +15,62 @@ function RouteComponent() {
   const { isAuthenticated } = useAuth0();
 
   return (
-    <Stack height="100%" justifyContent="center" alignItems="center">
-      <Stack spacing={5}>
-        <Stack spacing={2} maxWidth={1000}>
-          <Typography variant="h4" component="h1">
+    <Box height="100%" width="100%">
+      <Stack height="100%" width="100%" alignItems="center">
+        <Stack spacing={2} maxWidth={1000} marginY={8}>
+          <Typography variant="h1" component="h1">
             Create and Share Interactive Genomics Data Visualizations
           </Typography>
-          <Typography variant="h5" component="p">
+          <Typography variant="h2" component="p" sx={{ fontWeight: 300 }}>
             Join our community of researchers and data scientists to explore,
             visualize and collaborate on complex datasets.
           </Typography>
-          <Box>
-            {!isAuthenticated && (
-              <LoginButton variant="contained" color="primary" />
-            )}
+        </Stack>
+        <Stack spacing={5} width="100%" alignItems="center">
+          <Box width="90%">
+          <PublishedVisualizationGrid />
           </Box>
+          {!isAuthenticated && (
+            <Stack
+              sx={{ backgroundColor: "black" }}
+              paddingX={7}
+              paddingY={8}
+              width="90%"
+              borderRadius="16px"
+              direction="column"
+              spacing={4}
+            >
+              <Box>
+                <Typography variant="h1" component="p" color="#fff">
+                  Ready to dive in?
+                </Typography>
+                <Typography variant="h1" component="p" color="#fff">
+                  Start visualizing today.
+                </Typography>
+              </Box>
+              <Typography
+                color="#fff"
+                component="p"
+                variant="h4"
+                fontWeight={400}
+              >
+                Join our community of researchers and scientist to discover,
+                create and share biological data visualizations
+              </Typography>
+              <Box>
+                <LoginButton
+                  sx={{
+                    backgroundColor: "#fff",
+                    paddingX: "22px",
+                    paddingY: "16px",
+                    borderRadius: "10px",
+                  }}
+                />
+              </Box>
+            </Stack>
+          )}
         </Stack>
       </Stack>
-    </Stack>
+    </Box>
   );
 }
