cfec8a20cae213139b02d88bae8f0e6a6649b775
John Conroy <john-conroy@users.noreply.github.com>
2026-02-10 15:33:33 -0500

    Downgrade to React 18

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,3 +1,4 @@
+import path from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
@@ -5,5 +6,12 @@ import svgr from "vite-plugin-svgr";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [ TanStackRouterVite({ autoCodeSplitting: true }), react(), svgr()],
+  plugins: [TanStackRouterVite({ autoCodeSplitting: true }), react(), svgr()],
+  resolve: {
+    dedupe: ["react", "react-dom"],
+    alias: {
+      react: path.resolve(__dirname, "node_modules/react"),
+      "react-dom": path.resolve(__dirname, "node_modules/react-dom"),
+    },
+  },
 });
