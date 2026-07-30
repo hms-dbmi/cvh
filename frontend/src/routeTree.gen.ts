4b5244c85fe726287f3196cef5c0633cbb3d0207
John Conroy <john.ryan.conroy@gmail.com>
2026-07-30 13:21:52 -0400

    Add tutorials routes

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -13,6 +13,7 @@ import { Route as ProjectsRouteImport } from './routes/projects'
 import { Route as ProfileRouteImport } from './routes/profile'
 import { Route as IndexRouteImport } from './routes/index'
 import { Route as VisualizationsVisualizationIdRouteImport } from './routes/visualizations.$visualizationId'
+import { Route as TutorialsChar123SlugChar125RouteImport } from './routes/tutorials.{-$slug}'
 import { Route as ProjectChar123ProjectIdChar125RouteImport } from './routes/project.{-$projectId}'
 import { Route as ProjectVitessceChar123ProjectIdChar125RouteImport } from './routes/project.vitessce.{-$projectId}'
 
@@ -37,6 +38,12 @@ const VisualizationsVisualizationIdRoute =
     path: '/visualizations/$visualizationId',
     getParentRoute: () => rootRouteImport,
   } as any)
+const TutorialsChar123SlugChar125Route =
+  TutorialsChar123SlugChar125RouteImport.update({
+    id: '/tutorials/{-$slug}',
+    path: '/tutorials/{-$slug}',
+    getParentRoute: () => rootRouteImport,
+  } as any)
 const ProjectChar123ProjectIdChar125Route =
   ProjectChar123ProjectIdChar125RouteImport.update({
     id: '/project/{-$projectId}',
@@ -55,6 +62,7 @@ export interface FileRoutesByFullPath {
   '/profile': typeof ProfileRoute
   '/projects': typeof ProjectsRoute
   '/project/{-$projectId}': typeof ProjectChar123ProjectIdChar125Route
+  '/tutorials/{-$slug}': typeof TutorialsChar123SlugChar125Route
   '/visualizations/$visualizationId': typeof VisualizationsVisualizationIdRoute
   '/project/vitessce/{-$projectId}': typeof ProjectVitessceChar123ProjectIdChar125Route
 }
@@ -63,6 +71,7 @@ export interface FileRoutesByTo {
   '/profile': typeof ProfileRoute
   '/projects': typeof ProjectsRoute
   '/project/{-$projectId}': typeof ProjectChar123ProjectIdChar125Route
+  '/tutorials/{-$slug}': typeof TutorialsChar123SlugChar125Route
   '/visualizations/$visualizationId': typeof VisualizationsVisualizationIdRoute
   '/project/vitessce/{-$projectId}': typeof ProjectVitessceChar123ProjectIdChar125Route
 }
@@ -72,6 +81,7 @@ export interface FileRoutesById {
   '/profile': typeof ProfileRoute
   '/projects': typeof ProjectsRoute
   '/project/{-$projectId}': typeof ProjectChar123ProjectIdChar125Route
+  '/tutorials/{-$slug}': typeof TutorialsChar123SlugChar125Route
   '/visualizations/$visualizationId': typeof VisualizationsVisualizationIdRoute
   '/project/vitessce/{-$projectId}': typeof ProjectVitessceChar123ProjectIdChar125Route
 }
@@ -82,6 +92,7 @@ export interface FileRouteTypes {
     | '/profile'
     | '/projects'
     | '/project/{-$projectId}'
+    | '/tutorials/{-$slug}'
     | '/visualizations/$visualizationId'
     | '/project/vitessce/{-$projectId}'
   fileRoutesByTo: FileRoutesByTo
@@ -90,6 +101,7 @@ export interface FileRouteTypes {
     | '/profile'
     | '/projects'
     | '/project/{-$projectId}'
+    | '/tutorials/{-$slug}'
     | '/visualizations/$visualizationId'
     | '/project/vitessce/{-$projectId}'
   id:
@@ -98,6 +110,7 @@ export interface FileRouteTypes {
     | '/profile'
     | '/projects'
     | '/project/{-$projectId}'
+    | '/tutorials/{-$slug}'
     | '/visualizations/$visualizationId'
     | '/project/vitessce/{-$projectId}'
   fileRoutesById: FileRoutesById
@@ -107,6 +120,7 @@ export interface RootRouteChildren {
   ProfileRoute: typeof ProfileRoute
   ProjectsRoute: typeof ProjectsRoute
   ProjectChar123ProjectIdChar125Route: typeof ProjectChar123ProjectIdChar125Route
+  TutorialsChar123SlugChar125Route: typeof TutorialsChar123SlugChar125Route
   VisualizationsVisualizationIdRoute: typeof VisualizationsVisualizationIdRoute
   ProjectVitessceChar123ProjectIdChar125Route: typeof ProjectVitessceChar123ProjectIdChar125Route
 }
@@ -141,6 +155,13 @@ declare module '@tanstack/react-router' {
       preLoaderRoute: typeof VisualizationsVisualizationIdRouteImport
       parentRoute: typeof rootRouteImport
     }
+    '/tutorials/{-$slug}': {
+      id: '/tutorials/{-$slug}'
+      path: '/tutorials/{-$slug}'
+      fullPath: '/tutorials/{-$slug}'
+      preLoaderRoute: typeof TutorialsChar123SlugChar125RouteImport
+      parentRoute: typeof rootRouteImport
+    }
     '/project/{-$projectId}': {
       id: '/project/{-$projectId}'
       path: '/project/{-$projectId}'
@@ -163,6 +184,7 @@ const rootRouteChildren: RootRouteChildren = {
   ProfileRoute: ProfileRoute,
   ProjectsRoute: ProjectsRoute,
   ProjectChar123ProjectIdChar125Route: ProjectChar123ProjectIdChar125Route,
+  TutorialsChar123SlugChar125Route: TutorialsChar123SlugChar125Route,
   VisualizationsVisualizationIdRoute: VisualizationsVisualizationIdRoute,
   ProjectVitessceChar123ProjectIdChar125Route:
     ProjectVitessceChar123ProjectIdChar125Route,
