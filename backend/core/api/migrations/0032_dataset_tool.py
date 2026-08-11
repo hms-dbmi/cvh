71529792e40e9c10c0960ef7b7c18539671e44f9
John Conroy <john.ryan.conroy@gmail.com>
2026-08-11 10:05:55 -0400

    Update migration order

--- backend/core/api/migrations/0032_dataset_tool.py
+++ backend/core/api/migrations/0032_dataset_tool.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('api', '0030_dataset_processing_fields'),
+        ('api', '0031_alter_dataset_assembly_alter_dataset_cfdb_dcc_and_more'),
     ]
 
     operations = [
